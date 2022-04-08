0ea7884b250b62e2c523c549a8884ba7bad742a8
DanielRolfs <danielrolfs@icloud.com>
2022-04-08 17:41:42 +0200

    charts addet

---
+++ src/Data.js
@@ -0,0 +1,21 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2020,
+        userGain: 8000,
+        userLost: 823,
+    },
+    {
+        id: 2,
+        year: 2021,
+        userGain: 12934,
+        userLost: 2839,
+    },
+    {
+        id: 3,
+        year: 2022,
+        userGain: 5343,
+        userLost: 943,
+    },
+
+]
